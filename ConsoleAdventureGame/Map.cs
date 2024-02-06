ea0543c664f210b5924a043528422468af804f66
Paul Mould <PM902@student.aru.ac.uk>
2024-02-06 15:43:36 +0000

    Add project files.

---
+++ ConsoleAdventureGame/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdventureGame
+{
+    internal class Map
+    {
+        MapTile[,] map;
+
+        public Map()
+        {
+            map = new MapTile[5, 5];
+
+            map[0, 2] = new MapTile("Entrance Hall", false, false, false, true, 
+                "You are in a grand hall with towering pillars and faded tapestries adorning the walls. " +
+                "Sunlight filters in through cracks in the ceiling, casting eerie shadows on the ancient stone floor.");
+            map[1, 2] = new MapTile("Corridor of Echoes", false, false, true, true,
+                "You are in a long corridor lined with intricate carvings depicting scenes of ancient battles and mythical creatures." +
+                " Every step you take reverberates through the narrow passageway, creating an eerie echo that seems to follow you.");
+
+
+
+
+
+        }
+
+        
+
+
+    }
+}
---
+++ ConsoleAdventureGame/MapTile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdventureGame
+{
+    internal class MapTile
+    {
+        string roomName = "default";
+        string roomDescription = "default";
+        bool exitNorth;
+        bool exitSouth;
+        bool exitEast;
+        bool exitWest;
+
+        public MapTile(string name, bool north, bool south, bool east, bool west, string desc)
+        {
+            name = roomName;
+            desc = roomDescription;
+            north = exitNorth;
+            south = exitSouth;
+            east = exitEast;
+            west = exitWest;
+        }
+
+    }
+}
