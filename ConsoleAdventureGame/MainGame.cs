52ed08514eec55cd6307962e1767174846becb1f
Paul Mould <PM902@student.aru.ac.uk>
2024-02-07 12:07:34 +0000

    dunno

--- ConsoleAdventureGame/MainGame.cs
+++ ConsoleAdventureGame/MainGame.cs
@@ -14,56 +14,99 @@ namespace ConsoleAdventureGame
         Player player;
         MapTile currentTile;
         string noExit = "You cannot go that way!";
+        int selectedMenuItem;
+        List<string> actions = new List<string>() {"north", "south", "east", "west"};
+        int menuPosition = 11;
+        int movementNotificationPosition = 13;
+        int displayLocationPosition = 4;
 
         public MainGame() 
         {
             world = new Map();
             player = new Player();
+            Console.CursorVisible = false;
         }
 
         public void Run()
         {
             Console.WriteLine(DisplayLocation());
             HandleInput();
+            
         }
 
         private string DisplayLocation()
         {
+            Console.SetCursorPosition(0, displayLocationPosition);
             currentTile = world.map[player.locationX, player.locationY];
-            return currentTile.roomDescription;
+            string exits = "You can go ";
+            if (currentTile.exitNorth) { exits += "north "; }
+            if (currentTile.exitSouth) { exits += "south "; }
+            if (currentTile.exitWest) { exits += "west "; }
+            if (currentTile.exitEast) { exits += "east "; }
+            return $"{currentTile.roomName}\n{currentTile.roomDescription}\n\n{exits}";
         }
 
         private void HandleInput()
         {
-            string input;
-            input = Console.ReadLine();
-            if (input.ToLower().Contains("north") || input.ToLower() == "n")
+            Console.SetCursorPosition(0, menuPosition);
+            switch (selectedMenuItem)
             {
-                if(currentTile.exitNorth)
+                case 0:
+                    Console.WriteLine(">North  South  West  East");
+                    break;
+                case 1:
+                    Console.WriteLine(" North >South  West  East");
+                    break;
+                case 2:
+                    Console.WriteLine(" North  South >West  East");
+                    break;
+                case 3:
+                    Console.WriteLine(" North  South  West >East");
+                    break;
+                default:
+                    Console.WriteLine("ERROR: Selected Menu Item Not Found.");
+                    break;
+            }
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.Clear();
+            if (key.KeyChar.ToString().Equals("d"))
+            {
+                selectedMenuItem++;
+                if (selectedMenuItem >= actions.Count)
+                {
+                    selectedMenuItem = 0;
+                }
+            }
+            
+            if (key.KeyChar.ToString().Equals("a"))
+            {
+                selectedMenuItem--;
+                if (selectedMenuItem == -1 )
+                {
+                    selectedMenuItem = actions.Count -1;
+                }
+            }
+            
+            if (key.KeyChar.ToString().Equals(" "))
+            {
+                Console.SetCursorPosition(0, movementNotificationPosition);
+                if (selectedMenuItem == 0 && currentTile.exitNorth)
                 {
                     player.locationY--;
                     Console.WriteLine("You go North");
                 }
-                else
+                else if (selectedMenuItem == 1 && currentTile.exitSouth)
                 {
-                    Console.WriteLine(noExit);
+                   player.locationY++;
+                   Console.WriteLine("You go South");
                 }
-            }
-            if (input.ToLower().Contains("south") || input.ToLower() == "s")
-            {
-                if (currentTile.exitSouth)
+                else if (selectedMenuItem == 2 && currentTile.exitWest)
                 {
-                    player.locationY++;
-                    Console.WriteLine("You go South");
+                    player.locationX--;
+                    Console.WriteLine("You go West");
                 }
-                else
-                {
-                    Console.WriteLine(noExit);
-                }
-            }
-            if (input.ToLower().Contains("east") || input.ToLower() == "e")
-            {
-                if (currentTile.exitEast)
+                else if (selectedMenuItem == 3 && currentTile.exitEast)
                 {
                     player.locationX++;
                     Console.WriteLine("You go East");
@@ -72,28 +115,9 @@ namespace ConsoleAdventureGame
                 {
                     Console.WriteLine(noExit);
                 }
+                
             }
-            if (input.ToLower().Contains("west") || input.ToLower() == "w")
-            {
-                if (currentTile.exitWest)
-                {
-                    player.locationX--;
-                    Console.WriteLine("You go West");
-                }
-                else
-                {
-                    Console.WriteLine(noExit);
-                }
-            }
-
         }
-
-        private void DisplayUI()
-        {
-            Console.WriteLine();
-        }
-
-        
 
     }
 }
--- ConsoleAdventureGame/Map.cs
+++ ConsoleAdventureGame/Map.cs
@@ -19,12 +19,34 @@ namespace ConsoleAdventureGame
 
         public void AddRooms()
         {
-            map[0, 2] = new MapTile("Entrance Hall", false, false, false, true,
-                "You are in a grand hall with towering pillars and faded tapestries adorning the walls. " +
+            //main corridor
+            map[0, 2] = new MapTile("Entrance Hall", false, false, false, true,                                                      //Character limit kinda
+                "You are in a grand hall with towering pillars and faded tapestries adorning the walls.\n" +
                 "Sunlight filters in through cracks in the ceiling, casting eerie shadows on the ancient stone floor.");
             map[1, 2] = new MapTile("Corridor of Echoes", false, false, true, true,
-                "You are in a long corridor lined with intricate carvings depicting scenes of ancient battles and mythical creatures." +
-                " Every step you take reverberates through the narrow passageway, creating an eerie echo that seems to follow you.");
+                "You are in a long corridor lined with intricate carvings depicting scenes of ancient battles and mythical\n" +
+                "creatures. Every step you take reverberates through the narrow passageway, creating an eerie echo that\n" +
+                "seems to follow you.");
+            map[2, 2] = new MapTile("Chamber of Serenity", true, true, true, true, 
+                "A tranquil chamber bathed in soft, golden light. Fragrant incense fills the air, and a gentle breeze rustles\n" +
+                "through the colorful curtains that hang from the windows. It's a peaceful respite from the dangers lurking\n" +
+                "in the temple's depths.");
+            //go north to jump puzzle
+            map[2, 1] = new MapTile("jump room", true, true, false, false, "jump from this room to jump room 2");
+            map[2, 0] = new MapTile("jump room2", false, false, true, false, "cant go back to jump room 1 from here so go round");
+            map[1, 0] = new MapTile("backroom1 jump puzzle", false, true, false, true, "place a lever or something in this room to interact with");
+            map[1, 1] = new MapTile("backroom2 jump puzzle", false, true, false, true, "go south from here to go back to the old man (one way trip)");
+            //go east to door puzzle
+            map[3, 2] = new MapTile("Exit Room", false, false, true, false, "opens when both levers are pulled"); // set the east to true when the door is open
+            //go south to crouch puzzle
+            map[2, 3] = new MapTile("crouch room", true, false, false, true, "you can crouch to go east from here");
+            map[3, 3] = new MapTile("crouch room interactable", false, false, true, false, "place an interactable item in this room");
+            
+            
+            
+            
+            
+
 
         }
 
